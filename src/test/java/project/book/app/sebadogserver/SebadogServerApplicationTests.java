93b44dcdd8d2a5dddec39d452d45b400acfa085a
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-12-12 13:45:30 +0900

    [first] 초기 개발 환경 구축 #2
    1) 의존 라이브러리 설정
    2) 운영 전략에 따른 설정 파일 분리

---
+++ src/test/java/project/book/app/sebadogserver/SebadogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.book.app.sebadogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebadogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

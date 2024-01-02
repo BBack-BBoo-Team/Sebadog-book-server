1929ab2d26703790b0c6099f3b0abec38515a970
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-01-02 14:12:11 +0900

    [feat] CI/CD 구축을 위한 테스트 API 작성 #3
    자동화 배포의 성공여부 체크를 위해 테스트용 API를 만들었습니다.

--- src/main/java/project/book/app/sebadogserver/SebadogServerApplication.java
+++ src/main/java/project/book/app/sebadogserver/SebadogServerApplication.java
@@ -2,12 +2,19 @@ package project.book.app.sebadogserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SebadogServerApplication {
 
+    @GetMapping("hello")
+    public ResponseEntity hello() {
+        return ResponseEntity.ok("hello world");
+    }
     public static void main(String[] args) {
         SpringApplication.run(SebadogServerApplication.class, args);
     }

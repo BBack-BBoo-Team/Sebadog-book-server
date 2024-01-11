9eff297b5ae72d335de5d5e3bffc7e2102b5ad37
Hong ! <alswjd3813@naver.com>
2024-01-11 13:42:32 +0900

    [env] 초기 개발 환경 구축 #1

---
+++ src/test/java/com/book/app/SebadogBookServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebadogBookServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

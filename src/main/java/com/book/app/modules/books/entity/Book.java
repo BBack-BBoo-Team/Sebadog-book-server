b06d033623a8c4eb68b93bbe1651e6960df0f689
hongmj37 <alswjd3813@naver.com>
2024-01-17 17:21:34 +0900

    [feat] 책 entity 작성 #7

--- src/main/java/com/book/app/modules/books/entity/Book.java
+++ src/main/java/com/book/app/modules/books/entity/Book.java
@@ -1,7 +1,42 @@
 package com.book.app.modules.books.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.sql.Timestamp;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookId;
+
+    @Column(nullable = false)
+    private String bookTitle;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private String bookImg;
+
+    @Column(nullable = false)
+    private Timestamp createDt;
+
+    @Column(nullable = false)
+    private String createId;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column
+    private String finishDt;
 }

ef592e7d4fef295606981a4b00d75f1126647232
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-01-18 14:28:36 +0900

    [feat] Account 도메인 설계 #8
    공통 컬럼인 객체 생성일시와 출처를 JPA Auditing 기능을 활용하여 정보 저장

---
+++ src/main/java/com/book/app/modules/account/Account.java
@@ -0,0 +1,58 @@
+package com.book.app.modules.account;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(name = "ACCOUNT")
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ACCOUNT_ID")
+    private Long id;
+
+    @Column(name = "EMAIL", unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "NICKNAME", unique = true, nullable = false, length = 20)
+    private String nickname;
+
+    @CreatedDate
+    @Column(name = "CREATED_DT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "PROFILE_IMG")
+    private String profileImg;
+
+    protected Account() {}
+
+    private Account(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public static Account of(String email, String nickname) {
+        return new Account(email, nickname);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Account account)) return false;
+        return id != null && id.equals(account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
+

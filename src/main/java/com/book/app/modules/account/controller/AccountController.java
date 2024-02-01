186efcf0f5c5688f9537e627a2eaef4484e8bd26
Si-Hyeak-KANG <zlcls456@gmail.com>
2024-02-01 15:20:53 +0900

    [update] 각각의 API에 작성된 log 제거 #15
     - AOP로 분리

--- src/main/java/com/book/app/modules/account/controller/AccountController.java
+++ src/main/java/com/book/app/modules/account/controller/AccountController.java
@@ -1,5 +1,6 @@
-package com.book.app.modules.account;
+package com.book.app.modules.account.controller;
 
+import com.book.app.modules.account.Account;
 import com.book.app.modules.account.dto.AccountInfoDto;
 import com.book.app.modules.account.dto.SignUpDto;
 import com.book.app.modules.account.service.AccountService;
@@ -25,9 +26,7 @@ public class AccountController {
     // 사용자 정보 저장
     @PostMapping("/sign-up")
     public ResponseEntity<Response> signUp(@RequestBody @Valid SignUpDto info) {
-        log.info("sign-up info = {}",info);
         Account newAccount = accountService.saveSignUpInfo(info.toEntity());
-        log.info("newAccount = {}", newAccount);
         return new ResponseEntity<>(
                 Response.success(Domain.account, AccountInfoDto.from(newAccount)),
                 HttpStatus.CREATED);
@@ -36,9 +35,7 @@ public class AccountController {
     // 사용자 정보 조회
     @GetMapping("/info/{uid}")
     public ResponseEntity<Response> getInfoAccount(@PathVariable("uid") String uid) {
-        log.info("uid = {}", uid);
         Account getAccount = accountService.getAccountByUid(uid);
-        log.info("getAccount = {} ", getAccount);
         return new ResponseEntity<>(
                 Response.success(Domain.account, AccountInfoDto.from(getAccount)),
                 HttpStatus.OK);
